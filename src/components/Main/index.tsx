59e0e217aab11d165977e204fa646c5fcbf3b9f0
Moreno Antunes <morenomdz@gmail.com>
2021-05-02 18:26:06 -0300

    add styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>react av</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="img/logo.svg" alt="React avançado logo" />
+    <S.Title>react av</S.Title>
+    <S.Description>Curso React</S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="Programador de frente pra um pc"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
